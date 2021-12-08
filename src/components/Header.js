adb4f40cab900fa834b464e785174d4ea35a5038
Jun Choi <cjh4549@gmail.com>
2021-12-08 15:57:48 -0500

    added basic styling

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,12 +3,14 @@ const Header = ({ setInputValue, loading }) => {
 
     return(
         <header className="header">
-            <form className='form' onSubmit={e => {
-                e.preventDefault();
-                setInputValue(e.target.elements.query.value.toLowerCase());
-            }}>
-                <input className='form__input' type="text" name='query' placeholder='Search for GitHub Repositories'/>
-            </form>
+            <div className="wrapper">
+                <form className='form' onSubmit={e => {
+                    e.preventDefault();
+                    setInputValue(e.target.elements.query.value.toLowerCase());
+                }}>
+                    <input className='form__input' type="text" name='query' placeholder='Search for GitHub Repositories'/>
+                </form>
+            </div>
         </header>
     )
 
--- src/components/Main.js
+++ src/components/Main.js
@@ -2,24 +2,26 @@
 const Main = ({ repos, loading, error }) => {
     return(
         <main className='main'>
-            {loading && <div><h2>Searching...</h2></div>}
-            {error && <div><h2>Unexpected error occurred. Please try again.</h2></div>}
-            <ul className='repo'>
-                {repos.map( repo => {
-                    return(
-                        <li key={repo.id}>
-                            <a href={repo.html_url} target="_blank">{repo.name.toUpperCase()}</a>
-                            <p>{repo.description}</p>
-                            <ul className='repo__lists'>
-                                <li>Primary Language: {repo.language}</li>
-                                <li>Stars: {repo.stargazers_count}</li>
-                                <li>Forks: {repo.forks_count}</li>
-                                <li>Visibility: {repo.visibility}</li>
-                            </ul>
-                        </li>
-                    )
-                })}
-            </ul>
+            <div className="wrapper">
+                {loading && <div><h2 className="loading">Searching...</h2></div>}
+                {error && <div><h2 className="loading">Unexpected error occurred. Please try again.</h2></div>}
+                <ul className='repo'>
+                    {repos.map( repo => {
+                        return(
+                            <li className='individualRepo' key={repo.id}>
+                                <a href={repo.html_url} target="_blank">{repo.name.toUpperCase()}</a>
+                                <p>{repo.description}</p>
+                                <ul className='repo__lists'>
+                                    <li>Primary Language: {repo.language}</li>
+                                    <li>Stars: {repo.stargazers_count}</li>
+                                    <li>Forks: {repo.forks_count}</li>
+                                    <li>Visibility: {repo.visibility}</li>
+                                </ul>
+                            </li>
+                        )
+                    })}
+                </ul>
+            </div>
         </main>
     )
 }
